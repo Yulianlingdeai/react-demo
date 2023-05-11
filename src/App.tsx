fd744ed18462bb4cfe5d4e8883bfe77dc8b76a4f
yangxiaoyu <yangxiaoyu@xydtime.com>
2023-05-11 18:05:40 +0800

    feat: 新增demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import type { ChangeEvent } from "react";
+type state = {
+    name: string;
+    age: number | string;
+};
 function father() {
     return <div>father</div>;
 }
 class Son extends React.Component {
+    age = 18;
+    state: Readonly<state> = {
+        name: "yxy",
+        age: ""
+    };
+    handleChangeAge = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e);
+        this.setState({
+            age: +e.target.value + 1
+        });
+    };
     render() {
-        return <div>Son</div>;
+        const { name, age } = this.state;
+        return (
+            <>
+                <div>Son</div>
+                <div>{name}</div>
+                <div>{age}</div>
+                <div>{this.age}</div>
+                <input type="text" onChange={this.handleChangeAge} value={age} placeholder="请输入年龄" />
+            </>
+        );
     }
 }
 /**这是根组件 */
